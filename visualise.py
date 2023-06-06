21b38d8d8f4e8cb564baf0fa15a87da964f37b61
Alex Jago <4475543+alexjago@users.noreply.github.com>
2023-06-06 13:35:40 +1000

    typing, cleanup, formatting

--- visualise.py
+++ visualise.py
@@ -9,6 +9,11 @@ The primary methods that you'll want to call are `get_args` and `construct_svg`.
 """
 
 
+from typing import Tuple
+import sys
+import math
+from enum import Enum
+import argparse
 DEFAULT_CSS = """
 text {font-family: sans-serif; font-size: 10px; fill: #222;}
 text.label {filter: url(#keylineEffect); font-weight: bold}
@@ -27,28 +32,26 @@ text.label {filter: url(#keylineEffect); font-weight: bold}
 .bg {fill: #fff}
 """
 
-import argparse
-from enum import Enum
-import math
-import sys
-from typing import Tuple
 
 class Party(Enum):
     RED = ("Labor", "r")
     GREEN = ("Greens", "g")
-    BLUE = ("Coalition", "b") 
+    BLUE = ("Coalition", "b")
 
-### NOTE: throughout this file we'll use a variable called `A` to store our general state
-### This replaces the original and pervasive use of globals.
-### Default values are set in `get_args`
+# NOTE: throughout this file we'll use a variable called `A` to store our general state
+# This replaces the original and pervasive use of globals.
+# Default values are set in `get_args`
+
 
 def p2c(blue_pct: float, green_pct: float, A: argparse.Namespace) -> Tuple[float, float]:
     '''Percentages to Coordinates'''
     # trying to account for being out-of-frame here was worse than not doing it
     # additional context is needed and hence now line() exists
 
-    x = ((blue_pct - A.start) / (A.stop - A.start)) * A.inner_width + A.offset * A.scale
-    y = A.inner_width * (1 - ((green_pct - A.start) / (A.stop - A.start))) + A.scale
+    x = ((blue_pct - A.start) / (A.stop - A.start)) * \
+        A.inner_width + A.offset * A.scale
+    y = A.inner_width * (1 - ((green_pct - A.start) /
+                         (A.stop - A.start))) + A.scale
 
     return (x, y)
 
@@ -121,11 +124,10 @@ def calculate_winner(red_pct: float, green_pct: float, blue_pct: float, A: argpa
     # print("actual tie:", green_pct, red_pct, blue_pct, file=sys.stderr)
 
 
-
 def construct_dot(blue_pct: float, green_pct: float, A: argparse.Namespace) -> str:
     '''Given green and blue percentages, return an SVG fragment corresponding to a dot at the appropriate position.'''
     red_pct = 1.0 - (green_pct + blue_pct)
-    
+
     (x, y) = p2c(blue_pct, green_pct, A)
 
     tooltip_3cp = f"{Party.GREEN.value[0]}: {green_pct:.1%}, {Party.RED.value[0]}: {red_pct:.1%}, {Party.BLUE.value[0]}: {blue_pct:.1%}."
@@ -135,15 +137,15 @@ def construct_dot(blue_pct: float, green_pct: float, A: argparse.Namespace) -> s
         tooltip = f"{tooltip_3cp} Winner: {(winner.value)[0]} {margin:.1%}"
         return f'<circle cx="{x:g}" cy="{y:g}" r="{A.radius:g}" class="{(winner.value)[1]} d"><title>{tooltip}</title></circle>'.replace(".0%", "%")
 
-    except TypeError: # raised on a tie
+    except TypeError:  # raised on a tie
         tooltip = f"{tooltip_3cp} Winner: TIE"
         return f'<circle cx="{x:g}" cy="{y:g}" r="{A.radius:g}" class="t d"><title>{tooltip}</title></circle>'.replace(".0%", "%")
 
 
 def frange(start, stop=None, step=None) -> float:
     '''Floating-point range. [start = 0.0], stop, [step = 1.0]'''
-    start = float(start);
-    if not stop: # switcheroo
+    start = float(start)
+    if not stop:  # switcheroo
         stop = start
         start = 0.0
     if not step:
@@ -164,9 +166,11 @@ def clamp_val(val: float, lo: float, hi: float) -> float:
     """Constrain val to be between hi and lo"""
     return max(min(val, hi), lo)
 
+
 def clamp(val: float, A: argparse.Namespace) -> float:
     """Constrain val to be within A.start and A.stop"""
     return clamp_val(val, A.start, A.stop)
+
 
 def line(x0: float, y0: float, x1: float, y1: float, A: argparse.Namespace) -> str:
     """Takes two points (percentage-space) and returns the appropriate path fragment, ensuring that they're all in-bounds."""
@@ -193,10 +197,10 @@ def line(x0: float, y0: float, x1: float, y1: float, A: argparse.Namespace) -> s
         # whole of line would be off-viewport
         return ""
     else:
-        # get the line equation... 
-        m = (y1 - y0)/(x1 - x0) # gradient
+        # get the line equation...
+        m = (y1 - y0)/(x1 - x0)  # gradient
         c = y0 - m * x0         # y-offset
-        
+
         if x0 < A.start:
             ya = m * A.start + c
         elif x0 > A.stop:
@@ -222,24 +226,25 @@ def line(x0: float, y0: float, x1: float, y1: float, A: argparse.Namespace) -> s
     (xq, yq) = p2c(xb, yb, A)
 
     return f"M {xp:g} {yp:g} {xq:g} {yq:g}"
-        
+
+
 def draw_lines(A: argparse.Namespace) -> str:
     """Draw change-of-winner lines."""
 
-    # There are at least 8 points to draw lines between. 
+    # There are at least 8 points to draw lines between.
 
     # Firstly, a line #1-#2-#3
     # 1. Green vs Red on Y axis
     (x1, y1) = (A.start, (0.5 - (A.start * A.blue_to_green)))
 
     # 2. Green vs Rd midpoint. Controlled by ex-Blue split
-    # At max Greens-Red preferencing, it varies from 
+    # At max Greens-Red preferencing, it varies from
     # (0.25, 0.5) at full Blue-to-Red
     # degenerates at equal split (to terpoint)
     # (0.25, 0.25) at full Blue-to-Green
-    
-    # there's a line coming out of the terpoint that (at least for normal values) 
-    # marks out the "Greens 3CP >= Labor 3CP == Liberal 3CP" 
+
+    # there's a line coming out of the terpoint that (at least for normal values)
+    # marks out the "Greens 3CP >= Labor 3CP == Liberal 3CP"
     #   1 - (g+b) = b
 
     # and another coming in from #1 that A.marks the Labor-Greens 2CP boundary
@@ -267,7 +272,6 @@ def draw_lines(A: argparse.Namespace) -> str:
 
     (x2, y2) = (b, g)
 
-
     # 3. the (1/3, 1/3) point ("terpoint")
     # Always some sort of boundary
     (x3, y3) = (1.0/3.0, 1.0/3.0)
@@ -284,7 +288,7 @@ def draw_lines(A: argparse.Namespace) -> str:
         g = b
     (x4, y4) = (b, g)
 
-    # 5. Red vs Blue on X axis 
+    # 5. Red vs Blue on X axis
     (x5, y5) = (0.5 - A.start * A.green_to_blue, A.start)
 
     # Lines #3 - #4 - #5 represents the Red/Blue boundary
@@ -334,7 +338,7 @@ def draw_lines(A: argparse.Namespace) -> str:
         g = (1 - b)/2
 
     elif A.red_to_blue > 0.5:
-        # transpose of the < 0.5 case... 
+        # transpose of the < 0.5 case...
         g = A.red_to_blue / (2 - A.red_to_green)
         b = (1 - g)/2
 
@@ -345,7 +349,7 @@ def draw_lines(A: argparse.Namespace) -> str:
     (x7, y7) = (0.5, 0.5)
 
     # Lines #3 - #6 - #7 represents the Blue/Green boundary
-    blue_green = f'{line(x3, y3, x6, y6, A)} {line(x6, y6, x7, y7, A)}' 
+    blue_green = f'{line(x3, y3, x6, y6, A)} {line(x6, y6, x7, y7, A)}'
 
     # Unconditionally we also have a line down y = 1 - x
     # (this passes through the hapoint too, but no direction change)
@@ -356,6 +360,7 @@ def draw_lines(A: argparse.Namespace) -> str:
     # OK, time to draw all the lines!
 
     return f'\r\n<path d="{red_green} {red_blue} {blue_green} {top_right}" class="line" />\r\n'
+
 
 def draw_pois(A: argparse.Namespace) -> str:
     """Draw points of interest, as appearing in the specified CSV file"""
@@ -374,13 +379,15 @@ def draw_pois(A: argparse.Namespace) -> str:
 
             r2 = row[2] if len(row) > 2 else ""
             (x, y) = p2c(r0, r1, A)
-            tooltip = f"{r2}\n{Party.GREEN.value[0]}: {r1:.1%}, {Party.RED.value[0]}: {(1 - (r1+r0)):.1%}, {Party.BLUE.value[0]}: {r0:.1%}.".replace(".0%", "%")
+            tooltip = f"{r2}\n{Party.GREEN.value[0]}: {r1:.1%}, {Party.RED.value[0]}: {(1 - (r1+r0)):.1%}, {Party.BLUE.value[0]}: {r0:.1%}.".replace(
+                ".0%", "%")
 
             try:
                 (winner, margin) = calculate_winner(1 - (r0 + r1), r1, r0, A)
-                tooltip += f"\nWinner: {winner.value[0]} {margin:.1%}".replace(".0%", "%")
-            except TypeError: # ties A.n
-                tooltip += "\nWinner: TIE"                
+                tooltip += f"\nWinner: {winner.value[0]} {margin:.1%}".replace(
+                    ".0%", "%")
+            except TypeError:  # ties A.n
+                tooltip += "\nWinner: TIE"
             out += f'<circle cx="{x:g}" cy="{y:g}" r="{A.radius:g}" class="d poi"><title>{tooltip}</title></circle>\r\n'
 
         except (TypeError, IndexError, ValueError) as e:
@@ -388,6 +395,7 @@ def draw_pois(A: argparse.Namespace) -> str:
             print(row, file=sys.stderr)
 
     return out
+
 
 def construct_svg(A: argparse.Namespace) -> str:
     """Returns an SVG of the graph for given parameters as specified in `A`."""
@@ -428,7 +436,7 @@ def construct_svg(A: argparse.Namespace) -> str:
             {css} \
         ]]> \
         </style>' + \
-    '</defs>'
+        '</defs>'
 
     # place a bg rect
 
@@ -455,7 +463,7 @@ def construct_svg(A: argparse.Namespace) -> str:
     out += f'<text x="{A.width - A.scale*12:g}" y="{4*A.scale:g}" style="font-size:{A.scale:g}">{Party.GREEN.value[0]} to {Party.RED.value[0]}: {100.0*A.green_to_red:.1f}%</text>'
     out += f'<text x="{A.width - A.scale*12:g}" y="{6*A.scale:g}" style="font-size:{A.scale:g}">{Party.BLUE.value[0]} to {Party.RED.value[0]}: {100.0*A.blue_to_red:.1f}%</text>'
 
-    (x0, y0)   = p2c(A.start, A.start,  A)
+    (x0, y0) = p2c(A.start, A.start,  A)
     (x0, y100) = p2c(A.start, A.stop,   A)
     (x100, y0) = p2c(A.stop,  A.start,  A)
 
@@ -469,11 +477,9 @@ def construct_svg(A: argparse.Namespace) -> str:
             out += f'<path d="M {xpos:g} {ypos:g} h {-A.scale:g}" style="stroke: #222; stroke-width: {A.scale * 0.2:g}px"/>'
             out += f'<text y="{(ypos + A.scale/2):g}" x="{(xpos - 3*A.scale):g}" style="font-size:{A.scale:g}; text-anchor:right; text-align:middle">{g:.0%}</text>'
 
-
-    # Draw X axis 
+    # Draw X axis
     out += f'<path d="M {0:g} {y0:g} H {x100:g}" style="stroke: #222; stroke-width: {A.scale * 0.2:g}px" marker-end="url(#triangle)"/>'
     out += f'<text x="{A.width/2:g}" y="{y0 + 3.5*A.scale:g}" style="text-anchor:middle">{Party.BLUE.value[0]} 3CP</text>'
-
 
     for b in A.marks:
         if b > A.start and b <= (A.stop):
@@ -485,6 +491,7 @@ def construct_svg(A: argparse.Namespace) -> str:
     out += "</svg>"
 
     return out
+
 
 def get_args(args=None) -> argparse.Namespace:
     """pass args='' for defaults, or leave as None for checking argv.
@@ -498,23 +505,32 @@ def get_args(args=None) -> argparse.Namespace:
         N.B. all numeric values should be between zero and one.")
 
     parser.add_argument("--green-to-red", default=0.8, type=float,
-        help=f"{Party.GREEN.value[0]}-to-{Party.RED.value[0]} preference ratio (default: %(default)g)")
+                        help=f"{Party.GREEN.value[0]}-to-{Party.RED.value[0]} preference ratio (default: %(default)g)")
     parser.add_argument(f"--red-to-green", default=0.8, type=float,
-        help=f"{Party.RED.value[0]}-to-{Party.GREEN.value[0]} preference ratio (default: %(default)g)")
+                        help=f"{Party.RED.value[0]}-to-{Party.GREEN.value[0]} preference ratio (default: %(default)g)")
     parser.add_argument(f"--blue-to-red", default=0.7, type=float,
-        help=f"{Party.BLUE.value[0]}-to-{Party.RED.value[0]} preference ratio (default: %(default)g)")
-    parser.add_argument("--start", default=0.2, type=float, help="minimum X and Y axis value (default: %(default)g)")
-    parser.add_argument("--stop", default=0.6, type=float, help="maximum X and Y axis value (default: %(default)g)")
-    parser.add_argument("--step", default=0.01, type=float, help="precision of dots (default: %(default)g)")
-    parser.add_argument('--scale', default=10, type=int, help="pixels per percent (default: %(default)g)")
-    parser.add_argument('--offset', default=5, type=int, help="multiple of scale factor to A.offset axis by (default: %(default)g)")
-    parser.add_argument("--marks", nargs='+', default=[i/10.0 for i in range(0,10)], metavar="MARK", type=float, 
-        help="place axis marks at these values (default: every 10%%)")
-    parser.add_argument("--css", metavar='FILE', type=argparse.FileType('r'), help="Use CSS from specified file")
-    parser.add_argument("--input", "-i", help="input CSV of points of interest (format: x, y, label) (pass - for standard input)")
-    parser.add_argument("--output", "-o", type=argparse.FileType('w'), default=sys.stdout, help="output SVG (default: standard output)")
+                        help=f"{Party.BLUE.value[0]}-to-{Party.RED.value[0]} preference ratio (default: %(default)g)")
+    parser.add_argument("--start", default=0.2, type=float,
+                        help="minimum X and Y axis value (default: %(default)g)")
+    parser.add_argument("--stop", default=0.6, type=float,
+                        help="maximum X and Y axis value (default: %(default)g)")
+    parser.add_argument("--step", default=0.01, type=float,
+                        help="precision of dots (default: %(default)g)")
+    parser.add_argument('--scale', default=10, type=int,
+                        help="pixels per percent (default: %(default)g)")
+    parser.add_argument('--offset', default=5, type=int,
+                        help="multiple of scale factor to A.offset axis by (default: %(default)g)")
+    parser.add_argument("--marks", nargs='+', default=[i/10.0 for i in range(0, 10)], metavar="MARK", type=float,
+                        help="place axis marks at these values (default: every 10%%)")
+    parser.add_argument("--css", metavar='FILE',
+                        type=argparse.FileType('r'), help="Use CSS from specified file")
+    parser.add_argument(
+        "--input", "-i", help="input CSV of points of interest (format: x, y, label) (pass - for standard input)")
+    parser.add_argument("--output", "-o", type=argparse.FileType('w'),
+                        default=sys.stdout, help="output SVG (default: standard output)")
 
     return (parser.parse_args(args))
+
 
 def validate_args(A: argparse.Namespace) -> argparse.Namespace:
     # Clamp A.step to be in a reasonable range
@@ -528,20 +544,23 @@ def validate_args(A: argparse.Namespace) -> argparse.Namespace:
 
     # Calculate sizes...
     A.inner_width = A.scale * 100.0 * (A.stop - A.start)
-    A.width = (A.offset + 1) * A.scale + A.inner_width # extra on right and top
-    A.radius =  50.0 * A.scale * A.step # A.scale is pixels per percent, A.step is percent per dot
+    A.width = (A.offset + 1) * A.scale + \
+        A.inner_width  # extra on right and top
+    # A.scale is pixels per percent, A.step is percent per dot
+    A.radius = 50.0 * A.scale * A.step
 
     # Clamp our preference flows...
-    A.green_to_red  = max(min(abs(A.green_to_red),  1.0), 0.0)
-    A.red_to_green  = max(min(abs(A.red_to_green),  1.0), 0.0)
-    A.blue_to_red   = max(min(abs(A.blue_to_red),   1.0), 0.0)
+    A.green_to_red = max(min(abs(A.green_to_red),  1.0), 0.0)
+    A.red_to_green = max(min(abs(A.red_to_green),  1.0), 0.0)
+    A.blue_to_red = max(min(abs(A.blue_to_red),   1.0), 0.0)
 
     # Infer the inverse flows...
-    A.green_to_blue  = 1.0 - A.green_to_red
-    A.red_to_blue    = 1.0 - A.red_to_green
-    A.blue_to_green  = 1.0 - A.blue_to_red
+    A.green_to_blue = 1.0 - A.green_to_red
+    A.red_to_blue = 1.0 - A.red_to_green
+    A.blue_to_green = 1.0 - A.blue_to_red
 
     return A
+
 
 # the main show!
 if __name__ == "__main__":
